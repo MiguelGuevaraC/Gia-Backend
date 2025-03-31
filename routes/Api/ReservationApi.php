9969104bed29b53dbbfcbea3b42ed2ad4c0d0fd4
MiguelGuevaraC <mguevaracaj@unprg.edu.pe>
2025-03-31 11:26:10 -0500

    fix: se agrega de tipo put el api de actualizar recepcion

--- routes/Api/ReservationApi.php
+++ routes/Api/ReservationApi.php
@@ -7,7 +7,7 @@ Route::group(["middleware" => ["auth:sanctum"]], function () {
     Route::get('reservation', [RerservationController::class, 'index']);
     Route::post('reservation', [RerservationController::class, 'store']);
     Route::get('reservation/{id}', [RerservationController::class, 'show']);
-    Route::post('reservation/{id}', [RerservationController::class, 'update']);
+    Route::put('reservation/{id}', [RerservationController::class, 'update']);
     Route::delete('reservation/{id}', [RerservationController::class, 'destroy']);
 
 });
